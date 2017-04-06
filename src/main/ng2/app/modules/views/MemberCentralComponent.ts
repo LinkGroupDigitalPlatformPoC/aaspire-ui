5d265410a70195b363d7c3482f4816bc344fde9c
edhoi <edhoi@au1.ibm.com>
2017-04-05 18:30:26 -0700

    MemberCentral ready for API integration

--- src/main/ng2/app/modules/views/MemberCentralComponent.ts
+++ src/main/ng2/app/modules/views/MemberCentralComponent.ts
@@ -11,10 +11,17 @@ import './../common/RxJsOperators';
 })
 export class MemberCentralComponent {
 
+    searchResults : any;
       
     /**
      * TODO: Generic Type should be updated to only be extensions of an Entity interface.  
      */    
     constructor(private route: ActivatedRoute) {}
     
+    onSearch() {
+        console.log('MemberCentralComponent::onSearch');
+        // mock up serach result data should make a call to an api
+        this.searchResults = [{'membernum':'123456789','name':'John Doe','plan':'Standard','dob':'03-07-1979'},
+                         {'membernum':'123456777','name':'John Smith','plan':'Standard','dob':'03-07-1959'}];
+    }
 }
