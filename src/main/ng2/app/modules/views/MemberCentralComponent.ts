b7d20a807cf6bc49331e6f36dbebed54d3abe300
iancraig <iancraig@au1.ibm.com>
2017-04-12 18:05:29 +1000

    Use real call id - getting bad gateway - test this tomoprrow

--- src/main/ng2/app/modules/views/MemberCentralComponent.ts
+++ src/main/ng2/app/modules/views/MemberCentralComponent.ts
@@ -103,16 +103,18 @@ export class MemberCentralComponent {
 
         this.sharedService.currentEngagementBody = this.engagementBody;
 
+        var resultObj = JSON.parse(result);
+
         // trigger an event for any interested component/s
         // for the UI (see the HTML): populate the call structure with the member selected by the user
         let call: CallDetails = new CallDetails();
         call.memberId = parseInt(this.engagementBody.memberId);
-        call.callId = 987654321; // @ICtodo: get next available call identifier from add engagement API
+        call.callId = resultObj._id; // @ICtodo: get next available call identifier from add engagement API
         call.memberName = this.sharedService.currentMember.title + " " + this.sharedService.currentMember.givenName + " " + this.sharedService.currentMember.surname;
         this.contextMediatorService.onStartCall(call);
 
         // navigate to the call
-        this.router.navigateByUrl('/call/' + 1234567); // waiting for API team to return a call number
+        this.router.navigateByUrl('/call/' + resultObj._id); // waiting for API team to return a call number
     }
 
     /**
