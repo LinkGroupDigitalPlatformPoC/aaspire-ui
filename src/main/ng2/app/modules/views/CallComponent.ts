ad62e29fd070c1222941b1a46d38052a1daf0841
edhoi <edhoi@au1.ibm.com>
2017-04-12 16:16:37 -0700

    Fixed some errors after merge

--- src/main/ng2/app/modules/views/CallComponent.ts
+++ src/main/ng2/app/modules/views/CallComponent.ts
@@ -8,12 +8,12 @@ import './../common/RxJsOperators';
 import { CallDetails } from './../models/CallDetails';
 import { RefData } from './../models/RefData';
 import { RefDataValue } from './../models/RefDataValue';
-import { RefDataApi } from './../services/RefDataApi';
 import { IdentityCheck } from './../models/IdentityCheck';
 import { EngagementBody } from './../models/EngagementBody.interface';
 
 // services
 import { SharedService } from './../services/Shared.service'; // singleton
+import { RefDataApi } from './../services/RefDataApi';
 
 @Component({
     moduleId: module.id,
@@ -38,54 +38,52 @@ export class CallComponent implements OnInit {
     ngOnInit() {
         // TODO this should be an api to retrieve the call if the id was provided in the route
 
-        let engagement: EngagementBody = this.sharedService.currentEngagementBody;
-        console.log("CallComponent::ngOnInit(): current engagement = " + JSON.stringify(engagement));
-
-        this.call = new CallDetails(); // used in the HTML (for now)
-        this.call.callId = this.route.snapshot.params['id']; // from the URL
-        this.call.memberId = parseInt(engagement.memberId);
-        this.call.status = engagement.status;
-        this.call.startTime = engagement.dateTimeInitiated;
         
         this.callReasonsSelectItems = new Array<SelectItem>();
         this.identityChecks = new Array<IdentityCheck>();
         this.selectedIdentifiers = new Array<IdentityCheck>();
-        
         this.points = 0;
-        
-        // Identity checks
-        // this.sharedService.currentMember - contains member info (including identity check)
-        // name
-        this.identityChecks.push(new IdentityCheck('Name', 40, this.sharedService.currentMember.title + " " + 
-                    this.sharedService.currentMember.givenName + " " + this.sharedService.currentMember.surname));
-        // this.identityChecks.push(new IdentityCheck('Name', 40, 'John Smith'));
+        this.call = new CallDetails(); // used in the HTML (for now)
 
-        // DOB
-        this.identityChecks.push(new IdentityCheck('DOB', 40, this.sharedService.currentMember.dateOfBirth));
-        // this.identityChecks.push(new IdentityCheck('DOB', 40, '03-07-1985'));
+        if(this.route.snapshot.params['id']) {
+            let engagement: EngagementBody = this.sharedService.currentEngagementBody;
+            console.log("CallComponent::ngOnInit(): current engagement = " + JSON.stringify(engagement));
 
-        // checks in the identities array
-        for (let identity of this.sharedService.currentMember.identities) {
-            this.identityChecks.push(new IdentityCheck(identity.type, 30, identity.documentNumber));
+            this.call.callId = this.route.snapshot.params['id']; // from the URL
+            this.call.memberId = parseInt(engagement.memberId);
+            this.call.status = engagement.status;
+            this.call.startTime = engagement.dateTimeInitiated;
+
+            // Identity checks
+            // this.sharedService.currentMember - contains member info (including identity check)
+            // name
+            this.identityChecks.push(new IdentityCheck('Name', 40, this.sharedService.currentMember.title + " " + 
+                        this.sharedService.currentMember.givenName + " " + this.sharedService.currentMember.surname));
+            // this.identityChecks.push(new IdentityCheck('Name', 40, 'John Smith'));
+    
+            // DOB
+            this.identityChecks.push(new IdentityCheck('DOB', 40, this.sharedService.currentMember.dateOfBirth));
+            // this.identityChecks.push(new IdentityCheck('DOB', 40, '03-07-1985'));
+    
+            // checks in the identities array
+            for (let identity of this.sharedService.currentMember.identities) {
+                this.identityChecks.push(new IdentityCheck(identity.type, 30, identity.documentNumber));
+            }
+            // this.identityChecks.push(new IdentityCheck('Licence #', 30, 'L87239847'));
+            // this.identityChecks.push(new IdentityCheck('Passport #', 40, 'P789374985'));
+    
+            // last employer
+            this.identityChecks.push(new IdentityCheck('Last Employer', 40, this.sharedService.currentMember.lastEmployer));  
+            // this.identityChecks.push(new IdentityCheck('Last Employer', 40, 'Link Group'));       
         }
-        // this.identityChecks.push(new IdentityCheck('Licence #', 30, 'L87239847'));
-        // this.identityChecks.push(new IdentityCheck('Passport #', 40, 'P789374985'));
-
-        // last employer
-        this.identityChecks.push(new IdentityCheck('Last Employer', 40, this.sharedService.currentMember.lastEmployer));  
-        // this.identityChecks.push(new IdentityCheck('Last Employer', 40, 'Link Group'));       
-
         // getCallReason API call
         this.getCallReasons();
-        
     }
 
     setupCallReasonSelectItems(refData : RefData) {
-        for(let refDataValue of refData.values) {
-        //loop through and initialise the selectitem array with refdatavalues
-        for (let refDataValue of callReasons.values) {
+        for (let refDataValue of refData.values) {
             this.callReasonsSelectItems.push({label:refDataValue.descr,value:refDataValue.value});
-        }  
+        }
     }
 
     getCallReasons() {
